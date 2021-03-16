37846a458febb8941ccc14e8333a52f4a9b869c0
Arien Kock <arien.kock@gmail.com>
2021-03-16 19:56:03 +0100

    Fitbit link fix

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ router.get("/FitbitAuth", (req, res, next) => {
               FITBIT_CLIENT_ID
             )}&redirect_uri=${encodeURIComponent(
       FITBIT_REDIRECT_URI
-    )}&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
+    )}&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">Click to Authorize</a>
       `);
   }
 
