d9fce100b97199c48046dab24c97fec2dc9bc4d5
Arien Kock <arien.kock@gmail.com>
2021-03-16 19:45:12 +0100

    Fitbit OAuth

--- index.js
+++ index.js
@@ -8,18 +8,38 @@ const FileAsync = require("lowdb/adapters/FileAsync");
 const axios = require("axios");
 const qs = require("qs");
 
+/* Run command:
+
+env WITHINGS_CLIENT_ID='redacted' \
+    WITHINGS_CONSUMER_SECRET='redacted' \
+    WITHINGS_REDIRECT_URI='redacted' \
+    FITBIT_CLIENT_ID='redacted' \
+    FITBIT_CONSUMER_SECRET='redacted' \
+    FITBIT_REDIRECT_URI='redacted' \
+    npm run start
+
+*/
+
 const {
   WITHINGS_CLIENT_ID,
   WITHINGS_CONSUMER_SECRET,
   WITHINGS_REDIRECT_URI,
+  FITBIT_CLIENT_ID,
+  FITBIT_CONSUMER_SECRET,
+  FITBIT_REDIRECT_URI,
   TLS_PRIVATE_KEY_PATH,
   TLS_CERT_PATH,
   TLS_FULL_CHAIN_PATH,
 } = process.env;
 if (
-  [WITHINGS_CLIENT_ID, WITHINGS_CONSUMER_SECRET, WITHINGS_REDIRECT_URI].some(
-    (s) => !s
-  )
+  [
+    WITHINGS_CLIENT_ID,
+    WITHINGS_CONSUMER_SECRET,
+    WITHINGS_REDIRECT_URI,
+    FITBIT_CLIENT_ID,
+    FITBIT_CONSUMER_SECRET,
+    FITBIT_REDIRECT_URI,
+  ].some((s) => !s)
 ) {
   console.error("Missing environment variables.");
   process.exit(1);
@@ -30,7 +50,7 @@ const lowDb = low(adapter).then((db) => {
   return db.defaults({
     subscriptions: [],
     withingsTokens: {},
-    fitbitTokens: [],
+    fitbitTokens: {},
   });
 });
 
@@ -46,21 +66,18 @@ router.get("/WithingsAuth", (req, res, next) => {
   function showWithingsAuthPage() {
     res.send(`
           <h1>Welcome</h1>
-          <a href=https://account.withings.com/oauth2_user/authorize2?client_id=${encodeURIComponent(
+          <a href="https://account.withings.com/oauth2_user/authorize2?client_id=${encodeURIComponent(
             WITHINGS_CLIENT_ID
           )}&scope=user.metrics&redirect_uri=${encodeURIComponent(
       WITHINGS_REDIRECT_URI
-    )}&response_type=code&state=nil>Click to Authorize</a>
+    )}&response_type=code&state=nil">Click to Authorize</a>
       `);
   }
 
-  function showFitbitAuthPage() {}
-
   function exchangeForWithingsTokens() {
-    postAuthCode(req.query.code)
+    postWithingsAuthCode(req.query.code)
       .then(storeWithingsTokens)
-      .then(showFitbitAuthPage)
-      .then((result) => res.send(result))
+      .then(() => res.redirect("/withings-to-fitbit/FitbitAuth"))
       .catch((err) => next(err));
   }
 
@@ -74,7 +91,60 @@ router.get("/WithingsAuth", (req, res, next) => {
   }
 });
 
-function postAuthCode(code) {
+router.get("FitbitAuth", (req, res, next) => {
+  if (req.query.code) {
+    exchangeForFitbitTokens();
+  } else {
+    showFitbitAuthPage();
+  }
+
+  function showFitbitAuthPage() {
+    res.send(`
+            <h1>And now Fitbit</h1>
+            <a href="https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${encodeURIComponent(
+              FITBIT_CLIENT_ID
+            )}&redirect_uri=${encodeURIComponent(
+      FITBIT_REDIRECT_URI
+    )}&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
+      `);
+  }
+
+  function exchangeForFitbitTokens() {
+    postFitbitAuthCode(req.query.code)
+      .then(storeFitbitTokens)
+      .then(() => res.redirect("/withings-to-fitbit/Complete"))
+      .catch((err) => next(err));
+  }
+
+  function storeFitbitTokens(result) {
+    return lowDb.then((db) =>
+      db.get("fitbitTokens").set(result.data.body.user_id, result.data).write()
+    );
+  }
+});
+
+function postFitbitAuthCode(code) {
+  const data = qs.stringify({
+    clientId: FITBIT_CLIENT_ID,
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri: FITBIT_REDIRECT_URI,
+  });
+  return axios.post("https://api.fitbit.com/oauth2/token", data, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " + base64(FITBIT_CLIENT_ID + ":" + FITBIT_CONSUMER_SECRET),
+    },
+  });
+}
+
+function base64(data) {
+  let buff = new Buffer.from(data, "utf-8");
+  return buff.toString("base64");
+}
+
+function postWithingsAuthCode(code) {
   const data = qs.stringify({
     action: "requesttoken",
     grant_type: "authorization_code",
